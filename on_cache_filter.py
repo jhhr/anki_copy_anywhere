23492e647abec415fd690b129fd9665a360334dd
jhhr <jormki@gmail.com>
2024-05-12 17:04:58 +0300

    Add ignore_if_cached cache filter arg

--- on_cache_filter.py
+++ on_cache_filter.py
@@ -1,3 +1,4 @@
+import json
 import math
 import time
 from operator import itemgetter
@@ -5,9 +6,10 @@ from operator import itemgetter
 from anki.template import TemplateRenderContext
 from aqt import mw
 
+from .configuration import Config
 from .utils import write_custom_data, filter_init
 
-VALID_ARGS = ["cache_field", "only_empty"]
+VALID_ARGS = ["cache_field", "only_empty", "ignore_if_cached"]
 
 
 def on_cache_filter(
@@ -18,10 +20,14 @@ def on_cache_filter(
      {{cache[
          cache_field='cache_field';
          only_empty='only_empty(=False)'
+         ignore_if_cached='ignore_if_cached(=True)'
      ]:Field}}
         where Field is the field to be cached which can be the result of another
         custom filters.
         The field will be cached for the number of days set in the configuration.
+
+    With the default behaviour the field will be displayed from the cache if it exists
+    When rendering the card and the cache has expired, it is automatically updated.
     """
 
     if not (filter.startswith("cache[") and filter.endswith("]")):
@@ -29,13 +35,48 @@ def on_cache_filter(
 
     args_dict, is_cache, show_error_message = filter_init("cache", VALID_ARGS, filter, context)
 
-    cache_field, only_empty = itemgetter("cache_field", "only_empty")(args_dict)
+    (
+        cache_field,
+        only_empty,
+        ignore_if_cached
+    ) = itemgetter(
+        "cache_field",
+        "only_empty",
+        "ignore_if_cached"
+    )(args_dict)
 
     if cache_field is None:
         show_error_message("Error in cache[]: cache_field is required")
         return text
 
-    if not is_cache:
+    if ignore_if_cached is not None:
+        ignore_if_cached = ignore_if_cached.lower() == "true" or ignore_if_cached == "1"
+    else:
+        ignore_if_cached = True
+
+    config = Config()
+    config.load()
+
+    cache_is_expired = False
+    if ignore_if_cached and not is_cache:
+        # Check if the cache is still valid
+        card = context.card()
+        custom_data = card.custom_data
+        if custom_data != "":
+            custom_data = json.loads(custom_data)
+            if "fc" in custom_data:
+                cache_time = custom_data["fc"]
+                if cache_time is not None:
+                    cache_time = int(cache_time)
+                    if time.time() - cache_time < config.days_to_cache_fields_auto * 86400:
+                        # We don't return anything, so there isn't two sets of this data in the
+                        # template when viewing the card
+                        return ''
+                    else:
+                        # Cache is invalid, so we need to update it
+                        cache_is_expired = True
+
+    if not is_cache and not cache_is_expired:
         # We're not caching, so we don't need to do anything
         return text
 
@@ -48,6 +89,8 @@ def on_cache_filter(
     if cache_field is not None:
         try:
             cache_field_ord = context.note().keys().index(cache_field)
+            # only_empty can override the functionality of ignore_if_cached causing the card to be updated
+            # that's why the default only_empty is False and ignore_if_cached is True
             if only_empty and context.note().fields[cache_field_ord] != "":
                 return text
             context.note().fields[cache_field_ord] = text
