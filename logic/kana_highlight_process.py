5af60ef498ee314c8a99522ba2cf73b48b69085a
jhhr <jormki@gmail.com>
2025-06-18 22:34:13 +0300

    Require kanji_field to be found when given

--- logic/kana_highlight_process.py
+++ logic/kana_highlight_process.py
@@ -33,10 +33,15 @@ def kana_highlight_process(
 
     # Get the kanji to highlight and initial kanji data
     kanji_to_highlight = None
-    for name, field_text in note.items():
-        if name == kanji_field:
-            kanji_to_highlight = field_text
-            if kanji_to_highlight:
-                break
+    if kanji_field:
+        for name, field_text in note.items():
+            if name == kanji_field:
+                kanji_to_highlight = field_text
+                if kanji_to_highlight:
+                    break
+        if not kanji_to_highlight:
+            show_error_message(
+                f"Error in kana_highlight: kanji_field '{kanji_field}' not found in note."
+            )
 
     return kana_highlight(kanji_to_highlight, text, return_type, with_tags_def, show_error_message)
