cf052714b101ab8c326a5be3b1b361dc49061b32
jhhr <jormki@gmail.com>
2025-08-27 17:56:21 +0300

    Fix duplicate_note causing an error when note.id=0

--- utils/duplicate_note.py
+++ utils/duplicate_note.py
@@ -1,28 +1,18 @@
-from anki import notes_pb2
 from anki.notes import Note
+
+from copy import deepcopy
 
 
 def duplicate_note(note: Note) -> Note:
     """
-    Duplicate a note by creating a new instance and copying the fields.
-    Using copy.deepcopy on a Note object does not work, and Note(id=note.id) does not
-    work on a new note (where id is 0), thus this utility function.
+    Duplicate a note with deepcopy, copying everything except the col attribute
+    as that needs to identical to the original note
     """
-    dupe_note = Note(col=note.col, model=note.note_type())
+    new_note = type(note).__new__(type(note))
 
-    # Copied code from notes.py _to_backend_note method
-    # the method calls hooks.note_will_flush(self) which is not desired here
-    # This code may break if the Note class changes in the future.
-    backend_note = notes_pb2.Note(
-        id=note.id,
-        guid=note.guid,
-        notetype_id=note.mid,
-        mtime_secs=note.mod,
-        usn=note.usn,
-        tags=note.tags,
-        fields=note.fields,
-    )
-    # Calling internal method that is not part of the public API, so this may break if the
-    # Note class changes in the future.
-    dupe_note._load_from_backend_note(backend_note)
-    return dupe_note
+    for k, v in note.__dict__.items():
+        if k == "col":
+            setattr(new_note, k, note.col)
+        else:
+            setattr(new_note, k, deepcopy(v))
+    return new_note
